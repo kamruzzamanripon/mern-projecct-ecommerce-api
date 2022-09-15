634d79a7280ee85080a26ade7dbce69361814207
kamruzzamanripon <rkamruzzaman@gmail.com>
2022-09-16 03:27:40 +0600

    product api done

--- controllers/product.js
+++ controllers/product.js
@@ -110,3 +110,89 @@ exports.update = (req, res, next)=>{
     })
 }
 
+//Delete a Product
+exports.delete = (req, res, next)=>{
+    let product = req.product;
+
+    product.remove((err, result)=>{
+        if(err){
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }else{
+            return res.json({
+                message: "Product Deleted Successfully"
+            })
+        }
+    })
+}
+
+
+//Get Single Product info
+exports.read = (req, res, next)=>{
+    req.product.photo = undefined;
+
+    return res.json(req.product)
+}
+
+
+//Get all Product info
+exports.readAll = (req, res, next)=>{
+    let order = req.query.order ? req.query.order : "asc";
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+    let limit = req.query.limit ? req.query.limit : 6;
+
+    Product.find()
+        .select("-photo")
+        .populate("category")
+        .sort([ [sortBy, order] ])
+        .limit(limit)
+        .exec((err, products)=>{
+            if(err){
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }else{
+                return res.send(products)
+            }
+        })
+}
+
+//get Single Product Photo
+exports.getPhoto = (req,res,next)=>{
+    if(req.product.photo.data){
+        res.set("Content-Type", req.product.photo.contentType);
+        return res.send(req.product.photo.data)
+    }
+}
+
+//Get all Product Categories
+exports.getCategories =(req, res, next)=>{
+    Product.distinct("category", {}, (err, result)=>{
+        if(err){
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }else{
+            return res.json(result)
+        }
+    })
+}
+
+//Get related products
+exports.getRelatedProducts =(req, res, next)=>{
+    let limit = req.query.limit ? req.query.limit : 2;
+
+    Product.find({_id: {$ne: req.product}, category: req.product.category})
+        .limit(limit)
+        .populate("category", "_id name")
+        .exec((err, result)=>{
+            if(err){
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }else{
+                return res.send(result)
+            }
+        })
+}
--- routes/product.js
+++ routes/product.js
@@ -9,14 +9,36 @@ const productController = require('../controllers/product');
 //product create
 router.route('/product/create/:userId')
     .post(authController.requireSignIn, authController.isAuth, authController.isAdmin, productController.create)  
-    
+
+//product update and delete    
 router.route('/product/:productId/:userId')   
     .put(authController.requireSignIn, authController.isAuth, authController.isAdmin, productController.update) 
+    .delete(authController.requireSignIn, authController.isAuth, authController.isAdmin, productController.delete) 
 
+//read single product info
+router.route("/product/:productId")
+    .get(productController.read) 
+    
+//Read all Product
+router.route("/products")
+    .get(productController.readAll)    
+
+//get Product Photo
+router.route("/product/photo/:productId")
+    .get(productController.getPhoto)       
+
+//get Product Categories
+router.route("/product/categories")
+    .get(productController.getCategories)   
+    
+//Show related Product
+router.route("/products/related/:productId")
+    .get(productController.getRelatedProducts)     
+
+    
 
 //find user by id
 router.param('userId', userController.userById)  
-
 //find product by id
 router.param('productId', productController.productById)
     
